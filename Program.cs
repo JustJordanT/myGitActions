05b31f5a01a9599f548311715d3df857e39d52ae
JustJordanT <jordantay9014@gmail.com>
2021-10-09 11:08:37 -0500

    test linter

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myGitActions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!")
+        }
+    }
+}
